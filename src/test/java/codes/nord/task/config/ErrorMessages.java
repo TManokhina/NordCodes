edbd0d6353e76ac977378566c3e095308b5787e3
Manokhina Tatyana <tmanokhi@yandex.ru>
2025-12-03 06:14:31 +0300

    Create ErrorMessages for tests

---
+++ src/test/java/codes/nord/task/config/ErrorMessages.java
@@ -0,0 +1,10 @@
+package codes.nord.task.config;
+
+public class ErrorMessages {
+
+    public static final String RESULT_SHOULD_BE_ERROR = "result должен быть ERROR";
+    public static final String RESULT_SHOULD_BE_OK = "result должен быть OK";
+    public static final String MESSAGE_SHOULD_BE_PRESENT = "message должен присутствовать";
+    public static final String MESSAGE_SHOULD_BE_ABSENT = "message должен отсутствовать при успехе";
+
+}
