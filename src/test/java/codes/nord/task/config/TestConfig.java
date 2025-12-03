ccd9b359b743cbdc869069ee7182f99708093b95
Manokhina Tatyana <tmanokhi@yandex.ru>
2025-12-03 06:13:56 +0300

    Create TestConfig

---
+++ src/test/java/codes/nord/task/config/TestConfig.java
@@ -0,0 +1,8 @@
+package codes.nord.task.config;
+
+public interface TestConfig {
+    String BASE_URL = "http://localhost:8080";
+    int MOCK_PORT = 8888;
+    String API_KEY = "qazWSXedc";
+    String VALID_TOKEN = "12866E1516504186B11ACB7DE2EA9F78";
+}
